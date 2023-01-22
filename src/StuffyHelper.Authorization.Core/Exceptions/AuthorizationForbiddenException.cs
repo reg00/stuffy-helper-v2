4e2a5669b1903231e5d09b3ed286ae0ff9d66d24
reg00 <vgrte@mail.ru>
2023-01-23 01:23:25 +0500

    fix status codes, add fronend config

---
+++ src/StuffyHelper.Authorization.Core/Exceptions/AuthorizationForbiddenException.cs
@@ -0,0 +1,15 @@
+﻿namespace StuffyHelper.Authorization.Core.Exceptions
+{
+    public class AuthorizationForbiddenException : Exception
+    {
+        public AuthorizationForbiddenException(string message)
+                       : base(message)
+        {
+        }
+
+        public AuthorizationForbiddenException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
