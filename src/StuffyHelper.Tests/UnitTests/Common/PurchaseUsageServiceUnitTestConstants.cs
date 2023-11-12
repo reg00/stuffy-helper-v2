49501aea37953b2a05cf79273642217cafb96e99
reg00 <vgrte@mail.ru>
2023-11-12 19:15:17 +0500

    Add purchase usage unit tests

---
+++ src/StuffyHelper.Tests/UnitTests/Common/PurchaseUsageServiceUnitTestConstants.cs
@@ -0,0 +1,77 @@
+﻿using StuffyHelper.Core.Features.Common;
+using StuffyHelper.Core.Features.PurchaseUsage;
+
+namespace StuffyHelper.Tests.UnitTests.Common
+{
+    public static class PurchaseUsageServiceUnitTestConstants
+    {
+        public static PurchaseUsageEntry GetCorrectPurchaseUsageEntry()
+        {
+            return new()
+            {
+                Amount = 1,
+                Id = Guid.NewGuid(),
+                ParticipantId = Guid.NewGuid(),
+                Participant = new()
+                {
+                    UserId = "123"
+                },
+                PurchaseId = Guid.NewGuid(),
+            };
+        }
+
+        public static UpsertPurchaseUsageEntry GetCorrectAddPurchaseUsageEntry()
+        {
+            return new()
+            {
+                Amount = 5,
+                ParticipantId = Guid.NewGuid(),
+                PurchaseId = Guid.NewGuid(),
+            };
+        }
+
+        public static UpsertPurchaseUsageEntry GetCorrectUpdatePurchaseUsageEntry()
+        {
+            return new()
+            {
+                Amount = 6,
+                ParticipantId = Guid.NewGuid(),
+                PurchaseId = Guid.NewGuid(),
+            };
+        }
+
+        public static Response<PurchaseUsageEntry> GetCorrectPurchaseUsageResponse()
+        {
+            return new()
+            {
+                Total = 2,
+                TotalPages = 1,
+                Data = new List<PurchaseUsageEntry>()
+                {
+                    new()
+                    {
+                        Amount = 1,
+                        Id = Guid.NewGuid(),
+                        ParticipantId = Guid.NewGuid(),
+                        Participant = new()
+                        {
+                            UserId = "123"
+                        },
+                        PurchaseId = Guid.NewGuid(),
+                    },
+                    new()
+                    {
+                        Amount = 5,
+                        Id = Guid.NewGuid(),
+                        ParticipantId = Guid.NewGuid(),
+                        Participant = new()
+                        {
+                            UserId = "123"
+                        },
+                        PurchaseId = Guid.NewGuid(),
+                    },
+                }
+            };
+        }
+    }
+}
