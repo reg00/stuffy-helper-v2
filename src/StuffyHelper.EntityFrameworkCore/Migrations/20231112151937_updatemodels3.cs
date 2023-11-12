2202cbd1931226f4c5108c8e384c6542315280ea
reg00 <vgrte@mail.ru>
2023-11-12 20:19:45 +0500

    Update models context 2

---
+++ src/StuffyHelper.EntityFrameworkCore/Migrations/20231112151937_updatemodels3.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StuffyHelper.EntityFrameworkCore.Migrations
+{
+    public partial class updatemodels3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "event",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "event",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
