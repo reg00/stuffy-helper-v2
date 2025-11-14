3377edef37ffa19a9602376fe793e17b212b553f
reg00 <vgrte@mail.ru>
2025-11-14 21:51:03 +0500

    sudo

--- upgrade-stuffy-arm.sh
+++ upgrade-stuffy-arm.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Скрипт для перезапуска Docker Compose с обновлением образов
-set -e  # Завершить скрипт при любой ошибке
+sudo set -e  # Завершить скрипт при любой ошибке
 
-echo "🛑 Останавливаем Docker Compose..."
+sudo echo "🛑 Останавливаем Docker Compose..."
 sudo docker compose down
 
 
 # Удалить все dangling образы
-docker image prune -f
+sudo docker image prune -f
 
-echo "🔨 Собираем новые образы..."
+sudo echo "🔨 Собираем новые образы..."
 sudo docker build -f src/StuffyHelper.Authorization.Api/Dockerfile-arm -t slavadno/stuffy-auth-arm .
 sudo docker build -f src/StuffyHelper.Api/Dockerfile-arm -t slavadno/stuffy-core-arm .
 sudo docker build -f src/StuffyHelper.EmailService.Api/Dockerfile-arm -t slavadno/stuffy-email-arm .
 sudo docker build -f src/StuffyHelper.ApiGateway/Dockerfile-arm -t slavadno/stuffy-gateway-arm .
 
 
-echo "🚀 Запускаем Docker Compose..."
+sudo echo "🚀 Запускаем Docker Compose..."
 sudo docker compose up -d
 
-echo "✅ Готово! Контейнеры запущены с обновленными образами."
-echo "📋 Статус контейнеров:"
+sudo echo "✅ Готово! Контейнеры запущены с обновленными образами."
+sudo echo "📋 Статус контейнеров:"
 sudo docker-compose ps
